5ebf9fbffdf899af8a976510d4a086c5389011c4
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2026-03-09 20:33:49 +0530

    Change API base URL to relative path

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://43.205.253.229:9090/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
