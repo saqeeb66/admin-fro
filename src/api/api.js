3563db7da3a457e89d0584f7c7302bd5730d7639
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2026-03-11 15:53:23 +0530

    Update API base URL to production server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://arcot-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
